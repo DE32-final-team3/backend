97a48c0000a721eaba529a4fba7581387a68ba78
Nicou11 <lyoungin1204@gmail.com>
2024-11-22 12:47:30 +0900

    create_at 삭제

--- src/final_backend/user_schema.py
+++ src/final_backend/user_schema.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr, field_validator
+from pydantic import BaseModel, EmailStr
 from pydantic_core.core_schema import FieldValidationInfo
 
 
@@ -8,20 +8,6 @@ class UserCreate(BaseModel):
     email: EmailStr
     nickname: str
     password: str
-    password2: str
-
-    # 유효성 검증
-    @field_validator("nickname", "email", "password", check_fields=False)
-    def not_empty(cls, v):
-        if not v or not v.strip():
-            raise ValueError("빈 값은 허용되지 않습니다.")
-        return v
-
-    @field_validator("password2")
-    def passwords_match(cls, v, info: FieldValidationInfo):
-        if "password" in info.data and v != info.data["password"]:
-            raise ValueError("비밀번호가 일치하지 않습니다")
-        return v
 
 
 class Token(BaseModel):
@@ -33,9 +19,3 @@ class Token(BaseModel):
 class UserUpdate(BaseModel):
     nickname: str = None
     password: str = None
-
-    @field_validator("nickname", "password", check_fields=False)
-    def not_empty(cls, v):
-        if v and not v.strip():
-            raise ValueError("빈 값은 허용되지 않습니다.")
-        return v
